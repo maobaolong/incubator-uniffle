520a5da144861c62eb7917eb86eb655fb1bd5c8b
baoloongmao <baoloongmao@tencent.com>
2024-07-18 20:43:37 +0800

    Fix a double release issue about clearResource thread

--- server/src/main/java/org/apache/uniffle/server/buffer/ShuffleBufferManager.java
+++ server/src/main/java/org/apache/uniffle/server/buffer/ShuffleBufferManager.java
@@ -718,8 +718,17 @@ public class ShuffleBufferManager {
       Collection<ShuffleBuffer> buffers = bufferRangeMap.asMapOfRanges().values();
       if (buffers != null) {
         for (ShuffleBuffer buffer : buffers) {
-          buffer.release();
+          // the actual release size by this thread
+          long releaseSize = buffer.release();
           ShuffleServerMetrics.gaugeTotalPartitionNum.dec();
+          if (releaseSize != buffer.getSize()) {
+            LOG.warn(
+                "Release shuffle buffer size {} is not equal to buffer size {}, appId: {}, shuffleId: {}",
+                releaseSize,
+                buffer.getSize(),
+                appId,
+                shuffleId);
+          }
           releaseMemory(buffer.getSize(), false, false);
         }
       }
