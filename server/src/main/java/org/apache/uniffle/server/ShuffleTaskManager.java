86d23f9ffe20459d73d74de703dc642121def100
baoloongmao <baoloongmao@tencent.com>
2024-08-06 14:18:10 +0800

    Revert "Fix: cause double release when exceed limit after cache success"
    This reverts commit 4f016696f992700354c3a59503f0ecb8f5d1121a.

--- server/src/main/java/org/apache/uniffle/server/ShuffleTaskManager.java
+++ server/src/main/java/org/apache/uniffle/server/ShuffleTaskManager.java
@@ -495,6 +495,15 @@ public class ShuffleTaskManager {
     if (shuffleBufferManager.isHugePartition(partitionSize)) {
       shuffleTaskInfo.markHugePartition(shuffleId, partitionId);
     }
+    if (shuffleBufferManager.hasPartitionExceededHugeHardLimit(partitionSize)) {
+      throw new ExceedHugePartitionHardLimitException(
+          "Current partition size: "
+              + partitionSize
+              + " exceeded the huge hard limit size: "
+              + shuffleBufferManager.getHugePartitionSizeHardLimit()
+              + " after caching this shuffle data with size: "
+              + size);
+    }
   }
 
   public Roaring64NavigableMap getCachedBlockIds(String appId, int shuffleId) {
