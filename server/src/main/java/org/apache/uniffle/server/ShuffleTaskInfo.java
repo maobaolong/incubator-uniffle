7283d19c72ba347696ecdf47cf5a6709d04aeb45
baoloongmao <baoloongmao@tencent.com>
2024-09-21 08:55:30 +0800

    TX: [Server] Log more before server remove resource

--- server/src/main/java/org/apache/uniffle/server/ShuffleTaskInfo.java
+++ server/src/main/java/org/apache/uniffle/server/ShuffleTaskInfo.java
@@ -238,6 +238,10 @@ public class ShuffleTaskInfo {
     return partitionDataSizes.keySet();
   }
 
+  public Set<Integer> getPartitionIds(int shuffleId) {
+    return partitionDataSizes.get(shuffleId).keySet();
+  }
+
   public void incBlockNumber(int shuffleId, int partitionId, int delta) {
     long blockCount =
         this.partitionBlockCounters
