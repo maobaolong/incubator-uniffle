e34d3044985097e8fc85eafa8dbc0e3078573856
maobaolong <baoloongmao@tencent.com>
2024-10-12 11:48:01 +0800

    Revert ShuffleServerMetricsTest changes

--- server/src/test/java/org/apache/uniffle/server/ShuffleServerMetricsTest.java
+++ server/src/test/java/org/apache/uniffle/server/ShuffleServerMetricsTest.java
@@ -231,7 +231,7 @@ public class ShuffleServerMetricsTest {
     ObjectMapper mapper = new ObjectMapper();
     JsonNode actualObj = mapper.readTree(content);
     assertEquals(2, actualObj.size());
-    assertEquals(81, actualObj.get("metrics").size());
+    assertEquals(69, actualObj.get("metrics").size());
   }
 
   @Test
@@ -240,7 +240,7 @@ public class ShuffleServerMetricsTest {
     ObjectMapper mapper = new ObjectMapper();
     JsonNode actualObj = mapper.readTree(content);
     assertEquals(2, actualObj.size());
-    assertEquals(80, actualObj.get("metrics").size());
+    assertEquals(68, actualObj.get("metrics").size());
   }
 
   @Test
