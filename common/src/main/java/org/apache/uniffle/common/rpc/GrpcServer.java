604c86f02e5cc989316f250193c6327f0c60e5c4
baoloongmao <baoloongmao@tencent.com>
2024-10-25 15:29:31 +0800

    Revert "TX: Support config grpc queue size"
    This reverts commit 73ffad5f5442011d10a9457e7e04770e077bb64e.

--- common/src/main/java/org/apache/uniffle/common/rpc/GrpcServer.java
+++ common/src/main/java/org/apache/uniffle/common/rpc/GrpcServer.java
@@ -73,14 +73,13 @@ public class GrpcServer implements ServerInterface {
     this.grpcMetrics = grpcMetrics;
 
     int rpcExecutorSize = conf.getInteger(RssBaseConf.RPC_EXECUTOR_SIZE);
-    int queueSize = conf.getInteger(RssBaseConf.RPC_EXECUTOR_QUEUE_SIZE);
     pool =
         new GrpcThreadPoolExecutor(
             rpcExecutorSize,
             rpcExecutorSize * 2,
             10,
             TimeUnit.MINUTES,
-            Queues.newLinkedBlockingQueue(queueSize),
+            Queues.newLinkedBlockingQueue(Integer.MAX_VALUE),
             ThreadUtils.getThreadFactory("Grpc"),
             grpcMetrics);
     ThreadPoolManager.registerThreadPool(
