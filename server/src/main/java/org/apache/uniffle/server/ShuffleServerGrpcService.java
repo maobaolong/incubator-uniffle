d47afe793b23ff6e71d6a113cfc161f2591a2f70
maobaolong <baoloongmao@tencent.com>
2024-11-06 18:54:48 +0800

    [#2229] feat(server,client): Introduce OutputUtils class to reduce log size (#2230)
    ### What changes were proposed in this pull request?
    
    Introduce OutputUtils class to reduce log size
    
    ### Why are the changes needed?
    
    Fix: #2229
    
    ### Does this PR introduce _any_ user-facing change?
    
    Log become useful but tiny enough.
    
    ### How was this patch tested?
    
    new UT
    
    (cherry picked from commit 1364a828ef376109000032547e6c3361405770db)

--- server/src/main/java/org/apache/uniffle/server/ShuffleServerGrpcService.java
+++ server/src/main/java/org/apache/uniffle/server/ShuffleServerGrpcService.java
@@ -59,6 +59,7 @@ import org.apache.uniffle.common.rpc.ClientContextServerInterceptor;
 import org.apache.uniffle.common.rpc.StatusCode;
 import org.apache.uniffle.common.util.BlockIdLayout;
 import org.apache.uniffle.common.util.ByteBufUtils;
+import org.apache.uniffle.common.util.OutputUtils;
 import org.apache.uniffle.common.util.RssUtils;
 import org.apache.uniffle.proto.RssProtos;
 import org.apache.uniffle.proto.RssProtos.AppHeartBeatRequest;
@@ -717,6 +718,10 @@ public class ShuffleServerGrpcService extends ShuffleServerImplBase {
                 + ", partitionRequireSizesSize="
                 + request.getPartitionRequireSizesList().size();
       }
+      if (request.getPartitionIdsList() != null) {
+        auditArgs +=
+            ", partitionIds=" + OutputUtils.listToSegment(request.getPartitionIdsList(), 1, 10);
+      }
       auditContext.withArgs(auditArgs);
       StatusCode status = verifyRequest(appId);
       if (status != StatusCode.SUCCESS) {
@@ -998,8 +1003,14 @@ public class ShuffleServerGrpcService extends ShuffleServerImplBase {
               request.getBlockIdLayout().getTaskAttemptIdBits());
 
       auditContext.withAppId(appId).withShuffleId(shuffleId);
+      String partitionIdsOutput = OutputUtils.listToSegment(partitionsList, 1, 10);
       auditContext.withArgs(
-          "partitionsListSize=" + partitionsList.size() + ", blockIdLayout=" + blockIdLayout);
+          "partitionsListSize="
+              + partitionsList.size()
+              + ", partitionIds="
+              + partitionIdsOutput
+              + ", blockIdLayout="
+              + blockIdLayout);
 
       StatusCode status = verifyRequest(appId);
       if (status != StatusCode.SUCCESS) {
@@ -1018,7 +1029,7 @@ public class ShuffleServerGrpcService extends ShuffleServerImplBase {
       GetShuffleResultForMultiPartResponse reply;
       byte[] serializedBlockIds = null;
       String requestInfo =
-          "appId[" + appId + "], shuffleId[" + shuffleId + "], partitions" + partitionsList;
+          "appId[" + appId + "], shuffleId[" + shuffleId + "], partitions=" + partitionIdsOutput;
       ByteString serializedBlockIdsBytes = ByteString.EMPTY;
 
       try {
